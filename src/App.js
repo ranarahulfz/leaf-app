59c8ac0f770050502fd1a5feebd94e1a5ff30d52
Rahul Rana <rahulranafz@Rahuls-MacBook-Pro.local>
2019-09-08 09:44:22 +0530

    commit to add images and home page code.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar/Navbar'
+import Home from './components/home/Home'
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Home/>
     </div>
   );
 }
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,12 +1,152 @@
 import React from 'react'
 import './Home.css'
+import Tree from '../../assets/images/tree.svg'
+import Flower from '../../assets/images/flower.svg'
+import Rose from '../../assets/images/flower_images/rose.svg'
+import Sakura from '../../assets/images/flower_images/sakura.svg'
+import FlowerBouquet from '../../assets/images/flower_images/flower-bouquet.svg'
+import Herbs from '../../assets/images/flower_images/herbs.svg'
+import MapleLeaf from '../../assets/images/maple-leaf.svg'
+import DeveloperPic from '../../assets/images/developer/varun-mee.jpg'
+import GreenLeaf from '../../assets/images/green-leaf.svg'
 
 function Home () {
     return (
         <div className="home-container">
-            <section className="section#1">
-                
+            <section className="artboard-01">
+                    <img className="tree-background" src={Flower} alt="artboard"></img>
+                    <div className="">
+                        <h1 className="title center light-text">#HappyHealthyFlowers</h1>
+                        <h3 className="subtitle center light-text">Happiness radiates like the fragrance from a flower and draws 
+                        all good things towards you.</h3>
+                    </div>
             </section>
+            <section>
+                <div className="greenleaf-r-border"/>
+                <div className="cactus"/>
+                <div className="greenleaf-l-border"/>
+            </section>  
+            <section className="artboard-02">
+                <div className="container">
+                <div className="head">
+                    <h1 className="title dark-text">The Earth Laughs In Flower</h1>
+                    <h3 className="subtitle dark-text">Some of myriad benefits that you can expect from the simple act of adding flowers to your home.</h3>
+                </div>
+                <div className="rose-card">
+                <h4 className="top-text">Benefit 1</h4>
+                        <div className="card-img">
+                            <img src={Rose}/>
+                        </div>
+                        <h2 className="card-title">Reduce Stress</h2>
+                        <p className="card-subtitle">flowers were found to have the most stress-relieving impact.</p>
+                </div>
+                <div className="rose-card">
+                <h4 className="top-text">Benefit 2</h4>
+                        <div className="card-img">
+                        <img src={Rose}/>
+                        </div>
+                        <h2 className="card-title">Boost Memory</h2>
+                        <p className="card-subtitle">Flowers needed to unlock the mechanism in our brain.</p>
+                </div>
+                <div className="rose-card">
+                <h4 className="top-text">Benefit 3</h4>
+                        <div className="card-img">
+                        <img src={Rose}/>
+                        </div>
+                        <h2 className="card-title">Helps in Sleep</h2>
+                        <p className="card-subtitle">Lavender has long been believed to be a great, natural aid to sleep.</p>
+                </div>
+                </div>
+            </section>
+            <section>
+                <div className="greenleaf-r-border"/>
+                <div className="cactus"/>
+                <div className="greenleaf-l-border"/>
+            </section>  
+            <section className="artboard-03">
+                <div className="container-03">
+                    <div>
+                        <h1 className="title light-text">Edible flowers</h1>
+                        <h2 className="subtitle light-text">Not all flowers are safe to eat, but those that are can offer a unique burst of flavor and color to many dishes, including salads, sauces, beverages and entrées. Some of them may even offer health benefits.</h2>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <div className="greenleaf-r-border"/>
+                <div className="cactus"/>
+                <div className="greenleaf-l-border"/>
+            </section>  
+            <section className="section-04">
+                <div className="container-04">
+                    <div className="subcontainer">
+                        <div>
+                            <img className="artboard-04" src={Sakura}/>
+                        </div>    
+                        <div>
+                            <h1 className="title dark-text">Fragrant Flowers</h1>
+                            <h2 className="subtitle dark-text">Classic rose, calming lavender, intoxicating gardenia and delicate peony, the florist is a haven of natural perfumes.</h2>
+                        </div>
+                    </div>
+                    <div className="subcontainer">
+                        <div>
+                            <h1 className="title dark-text">Decoration Flowers</h1>
+                            <h2 className="subtitle dark-text">People plant them in gardens or arrange them in fresh flower bouquets to enjoy the beauty and fragrance of living blooms.</h2>
+                        </div>
+                        <div>
+                            <img className="artboard-04" src={FlowerBouquet}/>
+                        </div>    
+                    </div>
+                    <div className="subcontainer">
+                        <div>
+                            <img className="artboard-04" src={Herbs}/>
+                        </div>    
+                        <div>
+                            <h1 className="title dark-text">Medicine Flowers</h1>
+                            <h2 className="subtitle dark-text">Many flowers and other plant parts have purported medicinal properties, which have been used since ancient times.</h2>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <div className="greenleaf-r-border"/>
+                <div className="cactus"/>
+                <div className="greenleaf-l-border"/>
+            </section>  
+            <section className="section-05">
+                <div>
+                    <h2 class="section-05-quote">
+                        When you take a flower in your hand and really look at it, 
+                        it's your world for the moment. I want to give that world
+                        to someone else. Most people in the city around so, they 
+                        have no time to look at a flower. I want them to see it whether they want or not.
+                    </h2>
+                    <h3 class="section-05-author"> - Georgia O'Keeffe</h3>
+                </div>
+            </section>
+            <section>
+                <div className="greenleaf-r-border"/>
+                <div className="cactus"/>
+                <div className="greenleaf-l-border"/>
+            </section>  
+            <section className="section-06">
+                <div className="section-06-head">
+                    <h1 className="title center dark-text">Developer</h1>
+                </div>
+                <div className="subcontainer">
+                    <div className="developer-container">
+                        <img className="developer-pic" src={DeveloperPic}/>
+                    </div>
+                    <div className="developer-details">
+                        <h1 class="developer-name">Varun Rana</h1>
+                        <h2 class="developer-designation">CEO, 20kiwi</h2>
+                    </div>
+                </div>    
+            </section>
+            <section>
+                <div className="greenleaf-r-border"/>
+                <div className="cactus"/>
+                <div className="greenleaf-l-border"/>
+            </section>        
         </div>
     );
 }
